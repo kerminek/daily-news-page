c391949b078d21223a77e14781b14c0bf94b7089
kerminek <kermin0ps@gmail.com>
2023-04-16 17:25:40 +0200

    base path added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? "/portfolio-apps/news-app" : undefined,
+  basePath: isProd ? "/portfolio-apps/news-app" : undefined,
 };
 
 module.exports = nextConfig;
