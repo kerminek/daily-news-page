abdaa772fcb60657e3e7f9dc60263e5c76f54b25
kerminek <kermin0ps@gmail.com>
2023-04-16 19:18:48 +0200

    credits banner added, head tags added

---
+++ components/CreditsBanner.tsx
@@ -0,0 +1,11 @@
+const CreditsBanner = () => {
+  return (
+    <a className="text-xs text-gray-400 font-bold" href="https://jakubkrwawicz.pl" target="_blank">
+      <div className="text-center py-5 border-t hover:text-gray-500 transition duration-300 ease-in">
+        Jakub Krwawicz - 2023
+      </div>
+    </a>
+  );
+};
+
+export default CreditsBanner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import AgencyBanner from "@/components/AgencyBanner";
+import CreditsBanner from "@/components/CreditsBanner";
 import NasaComponent from "@/components/NasaComponent";
 import NytComponent from "@/components/NytComponent";
 import ReutersComponent from "@/components/ReutersComponent";
+import Head from "next/head";
 import Parser from "rss-parser";
 
 export default function Home(props: any) {
@@ -9,6 +11,11 @@ export default function Home(props: any) {
 
   return (
     <main className="sm:min-h-screen font-serif">
+      <Head>
+        <title>The News Page</title>
+        <meta name="author" content="Jakub Krwawicz" />
+        <meta name="description" content="The News Page - Your daily briefing." />
+      </Head>
       <section className="container mx-auto px-4 lg:px-8 pb-20 flex flex-wrap gap-x-4 lg:gap-x-8">
         {/* left side */}
         <NytComponent nyt={nyt} />
@@ -28,6 +35,7 @@ export default function Home(props: any) {
           </div>
         </div>
       </section>
+      <CreditsBanner />
     </main>
   );
 }
@@ -43,21 +51,24 @@ export async function getStaticProps() {
   );
 
   // for testing
-  // const nasaOldRes = {
-  //   hdurl: "https://apod.nasa.gov/apod/image/2304/hubble_ngc2419_potw1908a.jpg",
-  //   url: "https://apod.nasa.gov/apod/image/2304/hubble_ngc2419_potw1908a_1024.jpg",
-  //   explanation:
-  //     "Stars of the globular cluster NGC 2419 are packed into this Hubble Space Telescope field of view toward the mostly stealthy constellation Lynx. The two brighter spiky stars near the edge of the frame are within our own galaxy. NGC 2419 itself is remote though, some 300,000 light-years away. In comparison, the Milky Way's satellite galaxy, the Large Magellanic Cloud, is only about 160,000 light-years distant. Roughly similar to other large globular star clusters like Omega Centauri, NGC 2419 is intrinsically bright, but appears faint because it is so far away. Its extreme distance makes it difficult to study and compare its properties with other globular clusters that roam the halo of our Milky Way galaxy. Sometimes called \"the Intergalactic Wanderer\", NGC 2419 really does seem to have come from beyond the Milky Way. Measurements of the cluster's motion through space suggest it once belonged to the Sagittarius dwarf spheroidal galaxy, another small satellite galaxy being disrupted by repeated encounters with the much larger Milky Way.",
-  //   title: "NGC 2419: Intergalactic Wanderer",
-  //   copyright: 'Tunc Tezel',
-  // };
+  const nasaTestRes = {
+    hdurl: "https://apod.nasa.gov/apod/image/2304/hubble_ngc2419_potw1908a.jpg",
+    url: "https://apod.nasa.gov/apod/image/2304/hubble_ngc2419_potw1908a_1024.jpg",
+    explanation:
+      "Stars of the globular cluster NGC 2419 are packed into this Hubble Space Telescope field of view toward the mostly stealthy constellation Lynx. The two brighter spiky stars near the edge of the frame are within our own galaxy. NGC 2419 itself is remote though, some 300,000 light-years away. In comparison, the Milky Way's satellite galaxy, the Large Magellanic Cloud, is only about 160,000 light-years distant. Roughly similar to other large globular star clusters like Omega Centauri, NGC 2419 is intrinsically bright, but appears faint because it is so far away. Its extreme distance makes it difficult to study and compare its properties with other globular clusters that roam the halo of our Milky Way galaxy. Sometimes called \"the Intergalactic Wanderer\", NGC 2419 really does seem to have come from beyond the Milky Way. Measurements of the cluster's motion through space suggest it once belonged to the Sagittarius dwarf spheroidal galaxy, another small satellite galaxy being disrupted by repeated encounters with the much larger Milky Way.",
+    title: "NGC 2419: Intergalactic Wanderer",
+    copyright: "Tunc Tezel",
+  };
+
   console.log("Last time fetched: " + new Date().toTimeString());
+
+  const isProd = process.env.NODE_ENV === "production";
 
   return {
     props: {
       nyt: nytRes.items.slice(0, 11),
       reuters: reutersRes.items,
-      nasa: nasaRes,
+      nasa: isProd ? nasaRes : nasaTestRes,
     },
     revalidate: 60 * 15,
   };
