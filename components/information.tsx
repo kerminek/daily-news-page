1361841b8f6ffa24a257019470aeb27924434f89
kerminek <kermin0ps@gmail.com>
2024-01-12 16:08:02 +0100

    apos and quot building error fixed

--- components/information.tsx
+++ components/information.tsx
@@ -20,7 +20,7 @@ const InformationComponent = (props: Props) => {
     }, 1000);
 
     return () => clearInterval(intervalId);
-  }, [timer]);
+  }, [timer, popupStateSet]);
 
   return (
     <div
@@ -34,8 +34,8 @@ const InformationComponent = (props: Props) => {
       <div className="flex flex-col gap-10">
         <div className="text-center font-bold px-10 space-y-5">
           <p className="">
-            This page used to be fully dynamic, but due to the many problems with data providers, only NASA's "Picture
-            of the day" gets rehydrated.
+            This page used to be fully dynamic, but due to the many problems with data providers, only NASA&apos;s
+            &quot;Picture of the day&quot; gets rehydrated.
           </p>
           <p>Sorry.</p>
           <p className="font-normal text-xs italic">Click anywhere to close</p>
